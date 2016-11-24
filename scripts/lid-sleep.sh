7b1810c02bdf176f87472ccf4c39fa95fde2640b
Robert Baykov <baykovr@gmail.com>
2016-11-24 10:14:14 -0600

    Fix lid sleep script.

--- scripts/lid-sleep.sh
+++ scripts/lid-sleep.sh
@@ -11,17 +11,27 @@ function usage()
 	echo -e "\t" $0 "on    # set to sleep on lid close."
 	echo -e "\t" $0 "off   # set to do nothing on lid close."
 	echo -e "\t" $0 "apply # restart systemd-logind.service"
+	echo -e "\t" $0 "show  # show current setting in $conf_file"
 	exit 1
+}
+
+function show_cur()
+{
+	echo "[...] Current setting: " ; grep "HandleLidSwitch" $conf_file
 }
 
 function set_lid_behavior()
 {
 	if [ $1 == "on" ] ; then
-		sed -i '#/HandleLidSwitch=ignore/c\HandleLidSwitch=ignore' $conf_file
+		echo -e "[...] Uncommenting HandleLidSwitch=ignore"
+		sed -i '/HandleLidSwitch=ignore/c\HandleLidSwitch=ignore' $conf_file
 	elif [ $1 == "off" ] ; then
+		echo -e "[...] Commenting out HandleLidSwitch=ignore"
 		sed -i '/HandleLidSwitch=ignore/c\#HandleLidSwitch=ignore' $conf_file
 	elif [ $1 == "apply" ] ; then
 		systemctl restart systemd-logind.service
+	elif [ $1 == "show" ] ; then
+		show_cur
 	else
 		usage
 	fi
