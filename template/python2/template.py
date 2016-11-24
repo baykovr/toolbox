d2abd42ed84d03447a22fb43d2a1c26014f4d5f2
Robert Baykov <baykovr@gmail.com>
2016-11-24 10:20:54 -0600

    Merge template directories.

--- template/python2/template.py
+++ template/python2/template.py
@@ -1,7 +1,31 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+
+def cmd(command):
+	os.system(command)
+
+def pcmd(comand):
+    return os.popen(command).read()
+
+def perror(msg):
+	print('[ ! ] '+msg)
+	
+def plog(msg):
+	print('[...] '+msg)
 
 def main():
-	pass
+	parser = argparse.ArgumentParser(description='template')
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument( '-t',
+						'--template',
+						help='argument flag with no value',
+						action="store_true")
+	arguments = parser.parse_args()
+	
+	if arguments.template:
+		print('flag -t/--template found.')
 
 if __name__ == "__main__":
 	main()
